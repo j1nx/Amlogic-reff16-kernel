9304ea5172bdfa545fa89f0688ed123983b3f2b2
J1nx <admin@j1nx.nl>
2012-12-04 20:03:12 +0000

    Amlogic f16ref kernel source initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 5 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 Tue Dec 4 14:20:24 UTC 2012"
+#define LINUX_COMPILE_TIME "14:20:24"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "DataServer"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
